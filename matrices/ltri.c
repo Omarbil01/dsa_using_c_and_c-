b0d3d8723570981110f1c346f36d37505a6bd122
Omar <omarbilgrami50@gmail.com>
2025-01-10 12:45:27 +0530

    added lower triangular matrix file

---
+++ matrices/ltri.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+
+int main()
+{
+	int matrix[5][5];
+	int i,j;
+
+	for(i=0;i<5;i++)
+	{
+		for(j=0;j<5;j++)
+		{
+			if(i>=j)
+			{
+				printf("Enter index %i,%i: ",(i+1),(j+1));
+				scanf("%d",&matrix[i][j]);
+			}
+			else
+			{
+				matrix[i][j]=0;
+			}
+		}
+	}
+
+	printf("Lower triangular matrix: ");
+	printf("\n");
+	for(i=0;i<5;i++)
+	{
+		for(j=0;j<5;j++)
+		{
+			printf("%i ",matrix[i][j]);
+		}
+		printf("\n");
+	}
+
+	printf("Lower traingular elements: ");
+	for(i=0;i<5;i++)
+	{
+		for(j=0;j<5;j++)
+		{
+			if(i>=j)
+			{
+				printf("%i ",matrix[i][j]);
+			}
+		}
+	}
+	printf("\n");
+	return 0;
+}
