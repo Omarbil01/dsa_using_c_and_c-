44ba4d12833fef6e5591f141d9b27eae54b132d5
Omar <omarbilgrami50@gmail.com>
2025-01-16 19:23:54 +0530

    Added linked list code to insert at a certain index

---
+++ llist/llistinsert.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+	int data;
+	struct node *next;
+}* first=NULL;
+
+void set(int a[],int n)
+{
+	int i;
+	struct node *t,*last;
+
+	first=(struct node *)malloc(sizeof(struct node *));
+	first->data=a[0];
+	first->next=NULL;
+	last=first;
+
+	for(i=1;i<n;i++)
+	{
+		t=(struct node *)malloc(sizeof(struct node *));
+		t->data=a[i];
+		t->next=NULL;
+		last->next=t;
+		last =t;
+	}
+}
+
+void display(struct node *p)
+{
+	while(p != NULL)
+	{
+		printf("%d ",p->data);
+		p=p->next;
+	}
+}
+
+int count(struct node *p)
+{
+	int count = 0;
+	while(p != NULL)
+	{
+		count ++;
+		p= p->next;
+	}
+	return count;
+}
+
+void insert(struct node *p,int index,int x)
+{
+	int i;
+	struct node *t;
+	if(index<0 || index>count(p))
+	{
+		return;
+	}
+
+	t=(struct node *) malloc (sizeof(struct node));
+	t->data=x;
+
+	if(index == 0)
+	{
+		t->next=first;
+		first=t;
+	}
+	else
+	{
+		for(i=0;i<index-1;i++)
+		{
+			p=p->next;
+		}
+		t->next=p->next;
+		p->next=t;
+	}
+}
+
+int main()
+{
+	int x,y;
+	int a[]={33,44,27,11,78};
+
+	set(a,5);
+	display(first);
+
+	printf("\n");
+	printf("Enter the elements you want : ");
+	scanf("%d",&x);
+
+	printf("Enter the index you want to insert to: ");
+	scanf("%d",&y);
+
+	insert(first,y,x);
+	printf("Display after insertion: ");
+	display(first);
+
+	printf("\n");
+
+	free(first);
+
+	return 0;
+}
