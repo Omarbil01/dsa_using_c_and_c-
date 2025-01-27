d93d192a47f6405b87c28b29343f6a98966fcc02
Omar <omarbilgrami50@gmail.com>
2025-01-27 23:46:16 +0530

    Added doubly linked list code

---
+++ dllist/dllist.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+	struct node *prev;
+	int data;
+	struct node *next;
+}*first=NULL;
+
+void create(int a[],int n)
+{
+	struct node *t,*last;
+	int i;
+
+	first=(struct node *)malloc(sizeof(struct node));
+	first->data=a[0];
+	first->prev=first->next=NULL;
+	last=first;
+
+	for(i=1;i<n;i++)
+	{
+		t=(struct node *)malloc(sizeof(struct node));
+		t->data=a[i];
+		t->prev=last;
+		t->next=NULL;
+
+		last->next=t;
+		last=t;
+	}
+}
+
+void display(struct node *p)
+{
+	while(p!= NULL)
+	{
+		printf("%d ",p->data);
+		p=p->next;
+	}
+	printf("\n");
+}
+
+int main()
+{
+	int a[]={10,20,30,40,50};
+
+	create(a,5);
+
+	printf("Linked list: ");
+	display(first);
+	printf("\n");
+	return 0;
+}
