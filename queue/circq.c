b4f5bc10e6834222d89aea20060e905b4154ac05
Omar <omarbilgrami50@gmail.com>
2025-02-15 21:55:44 +0530

    Added circular queue program

---
+++ queue/circq.c
@@ -0,0 +1,80 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct queue
+{
+	int size;
+	int front;
+	int rear;
+	int *Q;
+};
+
+void create(struct queue *q,int size)
+{
+	q->size=size;
+	q->front=0;
+	q->rear=0;
+
+	q->Q=(int *)malloc(q->size*sizeof(int));
+}
+
+void enqueue(struct queue *q,int x)
+{
+	if((q->rear + 1)%q->size==q->front)
+	{
+		printf("Queue is full\n");
+	}
+
+	else
+	{
+		q->rear =(q->rear + 1)%q->size;
+		q->Q[q->rear]=x;
+	}
+}
+
+int dequeue(struct queue *q)
+{
+	int x=-1;
+
+	if(q->front == q->rear)
+	{
+		printf("Queue is empty\n");
+	}
+
+	else
+	{
+		q->front =(q->front + 1)%q->size;
+		x=q->Q[q->front];
+	}
+	return x;
+}
+
+void display(struct queue q)
+{
+	int i=q.front + 1;
+	do
+	{
+		printf("%d ",q.Q[i]);
+		i=(i+1)%q.size;
+	}while((i != q.rear +1)%q.size);
+}
+
+int main()
+{
+	struct queue q;
+	create(&q,5);
+
+	printf("Enqueue: \n");
+	enqueue(&q,5);
+	enqueue(&q,7);
+	enqueue(&q,9);
+	display(q);
+	printf("\n");
+
+	printf("Dequeue: \n");
+	int x=dequeue(&q);
+	printf("Deleted element: %d\n",x);
+	display(q);
+	printf("\n");
+	return 0;
+}
