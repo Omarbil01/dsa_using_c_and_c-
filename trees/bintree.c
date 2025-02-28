0012a18774da42c068e5a3c4a707247dd798ba75
Omar <omarbilgrami50@gmail.com>
2025-03-01 00:08:06 +0530

    Added binary tree program

---
+++ trees/bintree.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "tree.h"
+
+struct node *root=NULL;
+
+void treecreate()
+{
+	node *p;
+	node *r;
+	int x;
+	struct queue q;
+
+	create(&q,100);
+
+	printf("Enter root value: ");
+	scanf("%d",&x);
+	root=(struct node *)malloc(sizeof(struct node));
+	root->data=x;
+	root->lchild=NULL;
+	root->rchild=NULL;
+	enqueue(&q,root);
+
+	while(!isEmpty(q))
+	{
+		p=dequeue(&q);
+		printf("Enter value of left child of %d(Enter -1 if no value): ",p->data);
+		scanf("%d",&x);
+
+		if(x != -1)
+		{
+			r=(struct node *)malloc(sizeof(struct node));
+			r->data=x;
+			r->lchild=NULL;
+			r->rchild=NULL;
+			p->lchild=r;
+			enqueue(&q,r);
+		}
+
+		printf("Enter value of right child of %d(Enter -1 if no value): ",p->data);
+		scanf("%d",&x);
+
+		if(x != -1)
+		{
+			r=(struct node *)malloc(sizeof(struct node));
+			r->data=x;
+			r->lchild=NULL;
+			r->rchild=NULL;
+			p->rchild=r;
+			enqueue(&q,r);
+		}
+	}
+}
+
+void preorder(struct node *p)
+{
+	if(p != NULL)
+	{
+		printf("%d ",p->data);
+		preorder(p->lchild);
+		preorder(p->rchild);
+	}
+	printf("\n");
+}
+
+int main()
+{
+	treecreate();
+
+	printf("Preorder traversal: \n");	
+	preorder(root);
+	printf("\n");
+
+	return 0;
+}
