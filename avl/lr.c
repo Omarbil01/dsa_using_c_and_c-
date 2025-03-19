b63c946d271391522bd45029d7426b8330f9234e
Omarbil01 <112606217+Omarbil01@users.noreply.github.com>
2025-03-19 20:30:39 +0530

    Update lr.c

--- avl/lr.c
+++ avl/lr.c
@@ -153,7 +153,7 @@ int main()
     root = insert(root, 5);
     root = insert(root, 7);
     
-    printf("Inorder traversal of the AVL tree: ");
+    printf("Display: ");
     inorder(root);
     printf("\n");
     
