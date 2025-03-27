a5c6673b29a1fec93ba1919f07219fd28586fa88
Omar <omarbilgrami50@gmail.com>
2025-03-27 20:03:25 +0530

    Updated code to show easier version of delete function

--- heap/delete.c
+++ heap/delete.c
@@ -1,45 +1,44 @@
 #include <stdio.h>
 
-void insert(int a[], int *n, int key)
+void insert(int a[],int n)
 {
-    int i = *n;
-    a[i] = key;
-    (*n)++;
+    int i=n;
+    int temp;
+    temp=a[i];
 
-    while(i >= 1 && a[i] > a[(i-1)/2])
+    while(i>1 && temp>a[i/2])
     {
-        int temp = a[i];
-        a[i] = a[(i-1)/2];
-        a[(i-1)/2] = temp;
-
-        i = (i-1)/2;
+        a[i]=a[i/2];
+        i /= 2;
     }   
+    a[i]=temp;
 }
 
-int delete(int a[], int n)
+int delete(int a[],int n)
 {
-    int i, j, x, temp, val;
-    val = a[1];  
-    x = a[n];   
-    a[i] = a[n]; 
+    int i,j,x;
+    int val=a[1];
+    x=a[n];
+    a[1]=a[n];
+    a[n]=val;
 
-    i = 1;
-    j = i * 2;
+    i=1;
+    j=2*i;
 
-    while(j < n-1)
+    while(j<n - 1)
     {
-        if(a[j+1] > a[j])
+        if(a[j+1]>a[j])
         {
-            j = j+1;
+            j=j+1;
         }
 
-        if(a[i] < a[j])
+        if(a[i]<a[j])
         {
-            temp = a[i];
-            a[i] = a[j];
-            a[j] = temp;
-            i = j;
-            j = 2 * j;
+            int temp=a[i];
+            a[i]=a[j];
+            a[j]=temp;
+            i=j;
+            j=2*j;
         }
         else
         {
@@ -49,28 +48,23 @@ int delete(int a[], int n)
     return val;
 }
 
-void display(int a[], int n)
-{
-    printf("Max heap: ");
-    for(int i = 0; i < n; i++)
-    {
-        printf("%d ", a[i]);
-    }
-    printf("\n");
-}
-
 int main()
 {
-    int a[] = {0, 2, 5, 8, 9, 4, 10, 7};
-    int n = 8;
+    int a[]={0,10,20,30,25,5,40,35,0};
 
-    int key = 48;
-    insert(a, &n, key);
-
-    printf("Deleted value is %d\n", delete(a, 8));
+    for(int i=1;i<=7;i++)
+    {
+        insert(a,i);
+    }
 
     printf("Displaying: \n");
-    display(a, n);
-    
+    for(int i=1;i<=7;i++)
+    {
+        printf("%d ",a[i]);
+    }
+    printf("\n");
+
+    printf("Deleted value: %d\n",delete(a,7));
+
     return 0;
 }
