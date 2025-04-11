63f850da7691c82e4070d57c809f633a9495af66
Omar <omarbilgrami50@gmail.com>
2025-04-11 17:28:01 +0530

    Added queue using linked list header file for dfs and bfs programs

---
+++ graphs/queue.h
@@ -0,0 +1,65 @@
+#ifndef queue_h
+#define queue_h
+
+#include<stdlib.h>
+
+struct node
+{
+	int data;
+	struct node *next;
+}*front=NULL,*rear=NULL;
+
+void enqueue(int x)
+{
+	struct node *t;
+	t=(struct node *)malloc(sizeof(struct node));
+
+	if(t == NULL)
+	{
+		printf("Queue full \n");
+	}
+
+	else
+	{
+		t->data=x;
+		t->next=NULL;
+
+		if(front == NULL)
+		{
+			front = rear = t;
+		}
+
+		else
+		{
+			rear->next=t;
+			rear=t;
+		}
+	}
+}
+
+int dequeue()
+{
+	int x=-1;
+	struct node *t;
+
+	if(front == NULL)
+	{
+		printf("Queue empty \n");
+	}
+
+	else
+	{
+		x=front->data;
+		t=front;
+		front=front->next;
+		free(t);
+	}
+	return x;
+}
+
+int isEmpty()
+{
+	return front == NULL;
+}
+
+#endif
