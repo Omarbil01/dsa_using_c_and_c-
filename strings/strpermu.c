ad01c9f6b26b005fc5c771ba416f840bbdb89ac9
Omar <omarbilgrami50@gmail.com>
2025-05-16 23:46:00 +0530

    Added permutation of string program

---
+++ strings/strpermu.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+void perm(char s[],int k)
+{
+	static int a[10]={0};
+	//static is available in every function call
+	static char res[10];
+
+	int i;
+
+	if(s[k] == '\0')
+	{
+		res[k]='\0';
+		printf("%s\n",res);
+	}
+	else
+	{
+
+		for(int i=0;s[i] != '\0';i++)
+		{
+			if(a[i] == 0)
+			{
+				res[k]=s[i];
+				a[i]=1;
+				perm(s,k+1);
+				a[i]=0;
+			}
+		}
+	}
+}
+
+int main()
+{
+	char s[]="abc";
+	perm(s,0);
+
+	return 0;
+}
